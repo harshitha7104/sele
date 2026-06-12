26f22d33aadc5fc170377bcb7e1849416639ef00
harshitha7104 <harshithaacharya7104@gmail.com>
2026-06-12 19:21:01 +0000

    Fix package declaration in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,4 +1,4 @@
-import package com.example;
+package com.example;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
